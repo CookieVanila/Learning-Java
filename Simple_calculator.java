971fba20583b5c5baae756688e4cc75c83ef09de
Annisa Ardelia Setiawan - 2206059471 <annisa.ardelia@gmail.com>
2023-09-07 16:50:32 +0700

    Simple calculator with 2 variables

---
+++ Simple_calculator.java
@@ -0,0 +1,18 @@
+public class Simple_calculator{
+    public static void main(String[] args) {
+        Float a = 10.00f;
+        Float b = 5.00f;
+        System.out.print("Isi variable a adalah ");
+        System.out.println(a);
+        System.out.print("Isi variabel b adalah ");
+        System.out.println(b);
+        System.out.print("Hasil penjumlahan variabel a dan b adalah ");
+        System.out.println(a+b);
+        System.out.print("Hasil pengurangan variabel a dan b adalah ");
+        System.out.println(a-b);
+        System.out.print("Hasil perkalian variabel a dan b adalah ");
+        System.out.println(a*b);
+        System.out.print("Hasil pembagian variabel a dan b adalah ");
+        System.out.println(a/b);
+    }
+}
